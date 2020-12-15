d9d916e9e2be0b4ed5b9a3ce5c91befa5e0b5af4
Stéphane Lefebvre <stephane.lefebvre.ext@mappy.com>
2020-12-15 12:53:39 +0100

    [master] Black code

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
- 
+
 """Simple HTTP Server With Upload.
 This module builds on BaseHTTPServer by implementing the standard GET
 and HEAD requests in a fairly straightforward manner.
 see: https://gist.github.com/UniIsland/3346170
 """
- 
- 
+
+
 __version__ = "0.1"
 __all__ = ["SimpleHTTPRequestHandler"]
 __author__ = "bones7456"
 __home_page__ = "http://li2z.cn/"
- 
+
 import os
 import posixpath
 import http.server
@@ -23,10 +23,10 @@ import shutil
 import mimetypes
 import re
 from io import BytesIO
- 
- 
+
+
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
- 
+
     """Simple HTTP request handler with GET/HEAD/POST commands.
     This serves files from the current directory and any of its
     subdirectories.  The MIME type for files is determined by
@@ -35,9 +35,9 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     The GET/HEAD/POST requests are identical except that the HEAD
     request omits the actual contents of the file.
     """
- 
+
     server_version = "SimpleHTTPWithUpload/" + __version__
- 
+
     def do_GET(self, cookie=None):
         """Serve a GET request."""
         if len(self.path) < 2 and self.is_auth_normal_user1():
@@ -46,13 +46,18 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.path = "/normal_user2"
         if len(self.path) < 2 and self.is_auth_admin():
             self.path = "/admin"
-        if not "specialChar" in self.path and not ".png" in self.path and not self.is_auth() and not self.path.startswith("/chatroom"):
+        if (
+            not "specialChar" in self.path
+            and not ".png" in self.path
+            and not self.is_auth()
+            and not self.path.startswith("/chatroom")
+        ):
             self.path = "/"
         f = self.send_head(cookie)
         if f:
             self.copyfile(f, self.wfile)
             f.close()
- 
+
     def is_auth_normal_user1(self):
         cookies = self.get_cookies()
         return cookies.get("normal_user1") == "ok"
@@ -67,99 +72,105 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def is_auth(self):
         cookies = self.get_cookies()
-        return (cookies.get("normal_user1") == "ok") or (cookies.get("normal_user2") == "ok") or (cookies.get("admin") == "ok")
+        return (
+            (cookies.get("normal_user1") == "ok")
+            or (cookies.get("normal_user2") == "ok")
+            or (cookies.get("admin") == "ok")
+        )
 
     def do_HEAD(self):
         """Serve a HEAD request."""
         f = self.send_head()
         if f:
             f.close()
- 
+
     def extract_POST_data(self):
         retdict = {}
-        data = self.rfile.read(int(self.headers['Content-Length']))
-        for keyvalue in data.decode().split('&'):
-            key, value = keyvalue.split('=')
+        data = self.rfile.read(int(self.headers["Content-Length"]))
+        for keyvalue in data.decode().split("&"):
+            key, value = keyvalue.split("=")
             retdict[key] = value
         return retdict
 
     def get_cookies(self):
         retdict = {}
-        data = self.headers.get('Cookie')
+        data = self.headers.get("Cookie")
         if data is not None:
-            for keyvalue in data.split(';'):
-                key, value = keyvalue.split('=')
+            for keyvalue in data.split(";"):
+                key, value = keyvalue.split("=")
                 retdict[key.strip()] = value.strip()
         return retdict
-    
+
     def do_POST(self):
         """Serve a POST request."""
-        if not self.pathstartswith('/chatroom'):
-          if self.headers["Content-Type"] == "application/x-www-form-urlencoded":
-              data = self.extract_POST_data()
-              cookie = None
-              if data.get('psw') == "password":
-                  cookie = http.cookies.SimpleCookie()
-                  cookie['normal_user1'] = "ok"
-                  self.headers['Cookie'] = "normal_user1=ok" 
+        if not self.pathstartswith("/chatroom"):
+            if self.headers["Content-Type"] == "application/x-www-form-urlencoded":
+                data = self.extract_POST_data()
+                cookie = None
+                if data.get("psw") == "password":
+                    cookie = http.cookies.SimpleCookie()
+                    cookie["normal_user1"] = "ok"
+                    self.headers["Cookie"] = "normal_user1=ok"
 
-              if data.get('psw').replace(' ', "") == "'OR1=1--":
-                  cookie = http.cookies.SimpleCookie()
-                  cookie['normal_user2'] = "ok"
-                  self.headers['Cookie'] = "normal_user2=ok" 
+                if data.get("psw").replace(" ", "") == "'OR1=1--":
+                    cookie = http.cookies.SimpleCookie()
+                    cookie["normal_user2"] = "ok"
+                    self.headers["Cookie"] = "normal_user2=ok"
 
-              if data.get('psw') == "hardToGuessPassword":
-                  cookie = http.cookies.SimpleCookie()
-                  cookie['admin'] = "ok"
-                  self.headers['Cookie'] = "admin=ok" 
+                if data.get("psw") == "hardToGuessPassword":
+                    cookie = http.cookies.SimpleCookie()
+                    cookie["admin"] = "ok"
+                    self.headers["Cookie"] = "admin=ok"
 
-              self.do_GET(cookie)
-              return
-          r, info = self.deal_post_data()
-          print((r, info, "by: ", self.client_address))
-          f = BytesIO()
-          f.write(b'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-          f.write(b"<html>\n<title>Upload Result Page</title>\n")
-          f.write(b"<body>\n<h2>Upload Result Page</h2>\n")
-          f.write(b"<hr>\n")
-          if r:
-              f.write(b"<strong>Success:</strong>")
-          else:
-              f.write(b"<strong>Failed:</strong>")
-          f.write(info.encode())
-          f.write(("<br><a href=\"%s\">back</a>" % self.headers['referer']).encode())
-          f.write(b"<hr><small>Powerd By: bones7456, check new version at ")
-          f.write(b"<a href=\"http://li2z.cn/?s=SimpleHTTPServerWithUpload\">")
-          f.write(b"here</a>.</small></body>\n</html>\n")
-          length = f.tell()
-          f.seek(0)
-          self.send_response(200)
-          self.send_header("Content-type", "text/html")
-          self.send_header("Content-Length", str(length))
-          self.end_headers()
-          if f:
-              self.copyfile(f, self.wfile)
-              f.close()
+                self.do_GET(cookie)
+                return
+            r, info = self.deal_post_data()
+            print((r, info, "by: ", self.client_address))
+            f = BytesIO()
+            f.write(b'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+            f.write(b"<html>\n<title>Upload Result Page</title>\n")
+            f.write(b"<body>\n<h2>Upload Result Page</h2>\n")
+            f.write(b"<hr>\n")
+            if r:
+                f.write(b"<strong>Success:</strong>")
+            else:
+                f.write(b"<strong>Failed:</strong>")
+            f.write(info.encode())
+            f.write(('<br><a href="%s">back</a>' % self.headers["referer"]).encode())
+            f.write(b"<hr><small>Powerd By: bones7456, check new version at ")
+            f.write(b'<a href="http://li2z.cn/?s=SimpleHTTPServerWithUpload">')
+            f.write(b"here</a>.</small></body>\n</html>\n")
+            length = f.tell()
+            f.seek(0)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.send_header("Content-Length", str(length))
+            self.end_headers()
+            if f:
+                self.copyfile(f, self.wfile)
+                f.close()
         else:
-          # push the posts messages to the chat server HERE
-          pass
-        
+            # push the posts messages to the chat server HERE
+            pass
+
     def login(self):
         pass
 
     def deal_post_data(self):
-        content_type = self.headers['content-type']
+        content_type = self.headers["content-type"]
         if not content_type:
             return (False, "Content-Type header doesn't contain boundary")
         boundary = content_type.split("=")[1].encode()
-        remainbytes = int(self.headers['content-length'])
+        remainbytes = int(self.headers["content-length"])
         line = self.rfile.readline()
         remainbytes -= len(line)
         if not boundary in line:
             return (False, "Content NOT begin with boundary")
         line = self.rfile.readline()
         remainbytes -= len(line)
-        fn = re.findall(r'Content-Disposition.*name="file"; filename="(.*)"', line.decode())
+        fn = re.findall(
+            r'Content-Disposition.*name="file"; filename="(.*)"', line.decode()
+        )
         if not fn:
             return (False, "Can't find out file name...")
         path = self.translate_path(self.path)
@@ -169,10 +180,13 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         line = self.rfile.readline()
         remainbytes -= len(line)
         try:
-            out = open(fn, 'wb')
+            out = open(fn, "wb")
         except IOError:
-            return (False, "Can't create file to write, do you have permission to write?")
-                
+            return (
+                False,
+                "Can't create file to write, do you have permission to write?",
+            )
+
         preline = self.rfile.readline()
         remainbytes -= len(preline)
         while remainbytes > 0:
@@ -180,7 +194,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             remainbytes -= len(line)
             if boundary in line:
                 preline = preline[0:-1]
-                if preline.endswith(b'\r'):
+                if preline.endswith(b"\r"):
                     preline = preline[0:-1]
                 out.write(preline)
                 out.close()
@@ -189,7 +203,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                 out.write(preline)
                 preline = line
         return (False, "Unexpect Ends of data.")
- 
+
     def send_head(self, cookie=None):
         """Common code for GET and HEAD commands.
         This sends the response code and MIME headers.
@@ -201,15 +215,26 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         path = self.translate_path(self.path)
         f = None
         if os.path.isdir(path):
-            if any([elt in self.path for elt in ['Nicky', 'Andr', 'Florence', 'evg_olivier_fZQeD', 'Villefort']]):
+            if any(
+                [
+                    elt in self.path
+                    for elt in [
+                        "Nicky",
+                        "Andr",
+                        "Florence",
+                        "evg_olivier_fZQeD",
+                        "Villefort",
+                    ]
+                ]
+            ):
                 return self.list_images(path)
-            if not self.path.endswith('/'):
+            if not self.path.endswith("/"):
                 # redirect browser - doing basically what apache does
                 self.send_response(301)
                 self.send_header("Location", self.path + "/")
                 if cookie:
                     print("SENDING COOKIES IN HEADER")
-                    self.send_header("Set-Cookie", cookie.output(header='', sep=''))
+                    self.send_header("Set-Cookie", cookie.output(header="", sep=""))
                 self.end_headers()
                 return None
             for index in "index.html", "index.htm":
@@ -224,14 +249,14 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             # Always read in binary mode. Opening files in text mode may cause
             # newline translations, making the actual size of the content
             # transmitted *less* than the content-length!
-            f = open(path, 'rb')
+            f = open(path, "rb")
         except IOError:
             self.send_error(404, "File not found")
             return None
         self.send_response(200)
         if cookie:
             print("SENDING COOKIES IN HEADER")
-            self.send_header("Set-Cookie", cookie.output(header='', sep=''))
+            self.send_header("Set-Cookie", cookie.output(header="", sep=""))
         self.send_header("Content-type", ctype)
         fs = os.fstat(f.fileno())
         self.send_header("Content-Length", str(fs[6]))
@@ -249,13 +274,15 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         dirname = os.path.basename(path)
         replacement = ""
         f = BytesIO()
-        result = open('normal_user1/image.template', 'r').read()
+        result = open("normal_user1/image.template", "r").read()
         displaypath = cgi.escape(urllib.parse.unquote(self.path))
         for name in list:
             fullname = os.path.join(path, name)
-            if '.JPG' in fullname.upper():
-                replacement += '<img height="612" data-src="{}" src="blank.gif" class="lazy" alt="{}" title="{}">\n'.format('/normal_user1/'+ dirname + '/' + name, name, name)
-        result = result.replace('$@REPLACEME@$', replacement)
+            if ".JPG" in fullname.upper():
+                replacement += '<img height="612" data-src="{}" src="blank.gif" class="lazy" alt="{}" title="{}">\n'.format(
+                    "/normal_user1/" + dirname + "/" + name, name, name
+                )
+        result = result.replace("$@REPLACEME@$", replacement)
         f.write(result.encode())
         length = f.tell()
         f.seek(0)
@@ -265,7 +292,6 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         return f
 
- 
     def translate_path(self, path):
         """Translate a /-separated PATH to the local filename syntax.
         Components that mean special things to the local file system
@@ -273,19 +299,20 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         probably be diagnosed.)
         """
         # abandon query parameters
-        path = path.split('?',1)[0]
-        path = path.split('#',1)[0]
+        path = path.split("?", 1)[0]
+        path = path.split("#", 1)[0]
         path = posixpath.normpath(urllib.parse.unquote(path))
-        words = path.split('/')
+        words = path.split("/")
         words = [_f for _f in words if _f]
         path = os.getcwd()
         for word in words:
             drive, word = os.path.splitdrive(word)
             head, word = os.path.split(word)
-            if word in (os.curdir, os.pardir): continue
+            if word in (os.curdir, os.pardir):
+                continue
             path = os.path.join(path, word)
         return path
- 
+
     def copyfile(self, source, outputfile):
         """Copy all data between two file objects.
         The SOURCE argument is a file object open for reading
@@ -298,7 +325,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         to copy binary data as well.
         """
         shutil.copyfileobj(source, outputfile)
- 
+
     def guess_type(self, path):
         """Guess the type of a file.
         Argument is a PATH (a filename).
@@ -309,7 +336,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         as a default; however it would be permissible (if
         slow) to look inside the data to make a better guess.
         """
- 
+
         base, ext = posixpath.splitext(path)
         if ext in self.extensions_map:
             return self.extensions_map[ext]
@@ -317,28 +344,31 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         if ext in self.extensions_map:
             return self.extensions_map[ext]
         else:
-            return self.extensions_map['']
- 
-    if not mimetypes.inited:
-        mimetypes.init() # try to read system mime.types
-    extensions_map = mimetypes.types_map.copy()
-    extensions_map.update({
-        '': 'application/octet-stream', # Default
-        '.py': 'text/plain',
-        '.c': 'text/plain',
-        '.h': 'text/plain',
-        })
- 
- 
-def main(handler_class = SimpleHTTPRequestHandler,
-         server_class = http.server.ThreadingHTTPServer):
-    server_address = ('', 8080)
-    server_chatroom = ('', 8081)
-    httpd = server_class(server_address, handler_class)
-    #httpdChatroom = server_class(server_chatroom, handler_class)
-    httpd.serve_forever()
-    #httpdChatroom.serve_forever()
- 
-if __name__ == '__main__':
-    main()
+            return self.extensions_map[""]
 
+    if not mimetypes.inited:
+        mimetypes.init()  # try to read system mime.types
+    extensions_map = mimetypes.types_map.copy()
+    extensions_map.update(
+        {
+            "": "application/octet-stream",  # Default
+            ".py": "text/plain",
+            ".c": "text/plain",
+            ".h": "text/plain",
+        }
+    )
+
+
+def main(
+    handler_class=SimpleHTTPRequestHandler, server_class=http.server.ThreadingHTTPServer
+):
+    server_address = ("", 8080)
+    server_chatroom = ("", 8081)
+    httpd = server_class(server_address, handler_class)
+    # httpdChatroom = server_class(server_chatroom, handler_class)
+    httpd.serve_forever()
+    # httpdChatroom.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
